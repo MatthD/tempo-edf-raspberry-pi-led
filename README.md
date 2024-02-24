ab9fbd391dcc405ce42737370ede33c298a36796
MatthD <matthias.dieudonne@gmail.com>
2024-02-24 18:31:53 +0100

    feat(typescript): app in typescript bun seems to work

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # tempo-edf-raspberry-pi-led
-A simple multi langage raspberry pi set of tools to run on you raspberry pi  to show a RGB led color of the day
 
+A simple multi langage raspberry pi set of tools to run on you raspberry pi to show a RGB led color of the day
 
 ## Why
 
@@ -9,11 +9,14 @@ Because on red days we pay 3x the classical pricing so we need to know it by sho
 ## Project
 
 This project was a POC and work with a :
+
 - Rasperry pi 3 A+ model
 - [A kit of many things](https://www.amazon.fr/dp/B0BJF6TZJX?psc=1&ref=ppx_yo2ov_dt_b_product_details) (gpio board / wire / RGB led / a resitance of 320Ω)
 
 ## Langage
+
 I wanted to have this project in multi lang
+
 - Typescript because that's my favorite langage
 - Rust because i love to hate him
 - Go, to compare simplicity with Rust
@@ -22,6 +25,6 @@ I wanted to have this project in multi lang
 
   If you see thing badly wrote, don't hesitate to PR or issue me!
 
-  ## Schema
+## Schema
 
-  I used a brown wire for ground connected to N°6 entry based on this 
+I used a brown wire for ground connected to N°6 entry based on this
---
+++ typescript/main.ts
@@ -0,0 +1,58 @@
+import { BinaryValue, Gpio } from "onoff"; // Assuming you're using Node.js with npm install onoff
+import { CronJob } from "cron";
+import axios from "axios"; // Using Axios for HTTP requests
+
+type ApiCodeTempoResponse = {
+  dateJour: string;
+  codeJour: TarifColor; // Using 'TarifColor' instead of number
+  periode: string;
+};
+
+enum TarifColor {
+  Unknown = 0,
+  Blue = 1,
+  White = 2,
+  Red = 3,
+}
+
+// Fetch and set RGB color of the day
+const job = new CronJob("5 6,22 * * *", main);
+
+await main();
+job.start();
+
+async function main() {
+  const color = await fetchEDFColorOfTheDay();
+  if (color) {
+    const red = color === TarifColor.Red || color === TarifColor.White ? 1 : 0; // Assuming colors are returned with properties 'red', 'green' and 'blue'
+    const green = color === TarifColor.White ? 1 : 0;
+    const blue = color === TarifColor.Blue ? 1 : 0;
+
+    setRGBColor(red, green, blue);
+  } else {
+    console.error("No EDF color of the day available");
+  }
+}
+
+// Function to set RGB color
+function setRGBColor(red: BinaryValue, green: BinaryValue, blue: BinaryValue) {
+  const redPin = new Gpio(process.env.RED_PIN_NB, "out");
+  const greenPin = new Gpio(process.env.GREEN_PIN_NB, "out");
+  const bluePin = new Gpio(process.env.BLUE_PIN_NB, "out");
+  redPin.writeSync(red);
+  greenPin.writeSync(green);
+  bluePin.writeSync(blue);
+}
+
+// Function to get EDF color of the day (You'll need to replace this with your actual API endpoint)
+async function fetchEDFColorOfTheDay() {
+  try {
+    const response = await axios.get<ApiCodeTempoResponse>(
+      "https://www.api-couleur-tempo.fr/api/jourTempo/today"
+    ); // Replace 'https://your-edf-api/color' with the actual EDF color of the day API endpoint
+    return response.data.codeJour;
+  } catch (error) {
+    console.log(`Error fetching EDF color: ${error}`);
+    return null; // Return null if there was an error fetching the color
+  }
+}
